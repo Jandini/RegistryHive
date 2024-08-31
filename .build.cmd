61c02cf589ff91a4eef2d9ecd97553025e61d992
Matt Janda <matt.janda.kingston@gmail.com>
2024-08-31 01:18:27 +0100

    Use msbuild in build script

--- .build.cmd
+++ .build.cmd
@@ -1,2 +1,5 @@
-dotnet build src/RegistryHive.sln
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+
+msbuild src/RegistryHive.sln
 start bin/Debug/Viewer.exe res/other/SYSTEM
